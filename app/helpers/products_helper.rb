9c50e55e662c35af8796f56393480762b65eda41
LynchzDEV <opmok48@gmail.com>
2025-08-15 12:55:54 +0700

    Add product promotions and price display component

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,35 @@
 module ProductsHelper
+  PRODUCTS_ON_PROMOTION = [ 1, 2, 4 ]
+
+  def on_promotion?(product)
+    PRODUCTS_ON_PROMOTION.include?(product.id)
+  end
+
+  def products_on_promotion_ids
+    PRODUCTS_ON_PROMOTION
+  end
+
+  def promotion_price(product, discount_percentage = 20)
+    return nil unless product.price.present?
+
+    discounted_price = product.price * (1 - discount_percentage.to_f / 100)
+    discounted_price.round(2)
+  end
+
+  def discount_for_product(product)
+    case product.id
+    when 1 then 30
+    when 2 then 25
+    when 4 then 15
+    else 20
+    end
+  end
+
+  def promotion_price_custom(product)
+    return nil unless product.price.present? && on_promotion?(product)
+
+    discount = discount_for_product(product)
+    discounted_price = product.price * (1 - discount.to_f / 100)
+    discounted_price.round(2)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Product.destroy_all
+
+products = [
+  { name: "MacBook Pro M3", price: 2499 },
+  { name: "iPhone 15 Pro", price: 999 },
+  { name: "AirPods Pro", price: 249 },
+  { name: "iPad Air", price: 599 },
+  { name: "Apple Watch Ultra", price: 799 },
+  { name: "Mac Mini", price: 699 },
+  { name: "Magic Keyboard", price: 199 },
+  { name: "Apple TV 4K", price: 149 }
+]
+
+products.each do |product_data|
+  Product.create!(
+    name: product_data[:name],
+    price: product_data[:price]
+  )
+end
